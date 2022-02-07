586b47b9676fae3bfc17c2e9cdfdcab6f7822b26
p-lacerda <xcloudgameemail@gmail.com>
2022-02-07 02:26:57 -0300

    adiciona models e controllers

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser);
 
 app.listen(3000, () => console.log('ouvindo porta 3000!'));
 
 // não remova esse endpoint, e para o avaliador funcionar
-app.get('/', (request, response) => {
+app.get('/', (_request, response) => {
   response.send();
 });
+
+app.post('/user', '');
---
+++ models/User.js
@@ -0,0 +1,8 @@
+// displayName: DataTypes.STRING,
+// email: DataTypes.STRING,
+// password: DataTypes.STRING,
+// image: DataTypes.STRING,
+// }, {
+// sequelize,
+// modelName: 'User',
+// });
