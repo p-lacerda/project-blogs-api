68fe729d530852133e11054dcd0886e4e9627105
p-lacerda <xcloudgameemail@gmail.com>
2022-02-07 15:35:20 -0300

    model de user feito

--- models/User.js
+++ models/User.js
@@ -1,8 +1,19 @@
-// displayName: DataTypes.STRING,
-// email: DataTypes.STRING,
-// password: DataTypes.STRING,
-// image: DataTypes.STRING,
-// }, {
-// sequelize,
-// modelName: 'User',
-// });
+module.exports = (sequelize, DataTypes) => {
+    const Users = sequelize.define('Users', {
+        id: { 
+          type: DataTypes.INTEGER,
+          primaryKey: true,
+          autoIncrement: true, 
+        },
+        displayName: DataTypes.STRING,
+        email: { type: DataTypes.STRING, unique: true },
+        password: DataTypes.STRING,
+        image: DataTypes.STRING,
+      }, { timestamps: false });    
+
+    // Trabalhar no Inner Join de BlogPosts
+    // Users.associate = (models) => {
+    //     Users.hasMany(models.BlogPosts, { as: 'blogPosts', foreingKey: 'userId' });
+    //   };
+    return Users;
+  };
