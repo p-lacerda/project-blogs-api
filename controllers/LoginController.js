c360987582b1d552842b83ab729d7af705f32750
p-lacerda <xcloudgameemail@gmail.com>
2022-02-08 19:35:26 -0300

    update files

--- controllers/LoginController.js
+++ controllers/LoginController.js
@@ -17,7 +17,6 @@ const loginUser = async (req, res) => {
 
     res.status(200).json({ token });
   } catch (err) {
-    console.log(err);
     return res.status(err.code).json({ message: err.message });
   }
 };
